ddd181fc629f18bd14ff2fa6d4d3c284eff9d134
kevit-siddharth-singh <siddharth.singh@kevit.io>
2025-04-16 10:28:22 +0530

    fix:toggle menu close state fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -254,29 +254,29 @@ export default function Header() {
                     whileTap={{ scale: 0.9 }}
                     transition={{ duration: 0.2 }}
                 >
-                    <AnimatePresence mode="wait" initial={false}>
-                        {isMenuOpen ? (
-                            <motion.div
-                                key="close"
-                                initial={{ rotate: -90, opacity: 0 }}
-                                animate={{ rotate: 0, opacity: 1 }}
-                                exit={{ rotate: 90, opacity: 0 }}
-                                transition={{ duration: 0.2 }}
-                            >
-                                <X className="h-6 w-6" />
-                            </motion.div>
-                        ) : (
-                            <motion.div
-                                key="menu"
-                                initial={{ rotate: 90, opacity: 0 }}
-                                animate={{ rotate: 0, opacity: 1 }}
-                                exit={{ rotate: -90, opacity: 0 }}
-                                transition={{ duration: 0.2 }}
-                            >
-                                <Menu className="h-6 w-6" />
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
+
+                    {isMenuOpen ? (
+                        <motion.div
+                            key="close"
+                            initial={{ rotate: -90, opacity: 0 }}
+                            animate={{ rotate: 0, opacity: 1 }}
+                            exit={{ rotate: 90, opacity: 0 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <X className="h-6 w-6" />
+                        </motion.div>
+                    ) : (
+                        <motion.div
+                            key="menu"
+                            initial={{ rotate: 90, opacity: 0 }}
+                            animate={{ rotate: 0, opacity: 1 }}
+                            exit={{ rotate: -90, opacity: 0 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <Menu className="h-6 w-6" />
+                        </motion.div>
+                    )}
+
                 </motion.button>
             </div>
 
